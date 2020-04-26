e6d12f774a0f19369a7395d9d8828cb184827b19
EusebioAjas <eusebioajas6@gmail.com>
2020-04-26 01:54:09 -0500

    fun return string

---
+++ rustDesdeCero/gen_fun_str/box.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let a = 7;
+    let a_box: Box<i32>;
+
+    let mut a_ref: &i32 = &a;
+
+    println!("{} {}", a, *a_ref);
+    a_box = Box::new(a + 2);
+    a_ref = &*a_box;
+    println!(" {} {} {}", a, *a_ref, *a_box);
+}
---
+++ rustDesdeCero/gen_fun_str/fn_gen.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let a: i16 = f::<i16>('a', 37, 41);
+    let b: f64 = f::<f64>('b', 37.2, 41.1);
+
+    println!("{} {}", a, b);
+}
+
+fn f<T>(ch: char, num1: T, num2: T) -> T {
+    if ch == 'a' {
+        num1
+    } else {
+        num2
+    }
+}
